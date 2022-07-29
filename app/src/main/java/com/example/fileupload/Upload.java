938cff711c4b49da61c6743ba46ff2289452fac4
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-07-29 10:57:54 +0300

    This will add the pdffile upload methods

---
+++ app/src/main/java/com/example/fileupload/Upload.java
@@ -0,0 +1,30 @@
+package com.example.fileupload;
+
+public class Upload {
+    public String name;
+    public String url;
+
+    public Upload(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
+
+    public Upload() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
