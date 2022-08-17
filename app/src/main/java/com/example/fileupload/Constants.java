2511aae1d52d0f0c17075fe507be5e8422bb995c
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-08-17 10:42:59 +0300

    This will remove the action bar

--- app/src/main/java/com/example/fileupload/Constants.java
+++ app/src/main/java/com/example/fileupload/Constants.java
@@ -6,4 +6,6 @@ public class Constants {
 
     public static final String STORAGE_PATH_UPLOADS_images= "Images/";
     public static final String DATABASE_PATH_UPLOADS_images="Images";
+
+//    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
 }
