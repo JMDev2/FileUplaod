fdb8f172fb7de3506a4d258ed53fff9cf630d444
JOSEPHMAINA1995 <mainajoseph964@gmail.com>
2022-09-02 11:29:14 +0300

    This will add the viewdPdf activity, recyclerview and the xml filr

---
+++ app/src/main/java/com/example/fileupload/ViewPdfFilesActivity.java
@@ -0,0 +1,14 @@
+package com.example.fileupload;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewPdfFilesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pdf_files);
+    }
+}
